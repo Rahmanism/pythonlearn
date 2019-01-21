d490b07c880bb07e5dcb0a36987ff75569a4c6b3
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-01-21 21:59:06 +0330

    character_counter.py added

---
+++ character_counter.py
@@ -0,0 +1,20 @@
+# We want to know how many from each character is in the following string.
+myString = 'This is just a test text for learning dictionary in python.'
+
+counter  = dict()
+
+for letter in myString:
+    counter[letter] = counter.get(letter, 0) + 1
+    # the above line does the work of next four lines.
+    #if letter in counter:
+    #    counter[letter] += 1
+    #else:
+    #    counter[letter] = 1
+
+for i in counter:
+    print(i, ': ', end='')
+    for j in range(counter[i]):
+        print('+', end='')
+    print('> %d' % counter[i])
+
+    
