06dbe192a7dbe241115c6c5d37e019a8fe277306
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-01-22 13:31:32 +0330

    csvlearn.py added.

---
+++ csvlearn.py
@@ -0,0 +1,16 @@
+# read data from csv
+import csv
+from statistics import mean
+
+f = open('grades.csv')
+reader = csv.reader(f)
+grades = dict()
+for row in reader:
+    these_grades = list()
+    for grade in row[1:]:
+        these_grades.append(int(grade))
+        grades[row[0]] = these_grades
+
+print('Averages:................')
+for item in grades:
+    print('%12s => %.2f' % (item, mean(grades[item])))
