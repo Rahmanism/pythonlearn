225a0e33f086497d5b5f9e8c72f77380b3eada0d
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-01-23 19:58:55 +0330

    It 83% works. worldcup.txt is added that contains some examples.

--- 01_05_worldcup.py
+++ 01_05_worldcup.py
@@ -1,5 +1,4 @@
 # Chaper 01 - Unit 05 - Worl Cup Group B standings
-import sys
 
 """
 Order of games:
@@ -27,6 +26,7 @@ def calc(first, second, result):
     first['goal difference'] += x - y
     second['goal difference'] += y - x
 
+
 def calc_points(s):
     for i in s:
         s[i]['points'] = s[i]['wins'] * 3 + s[i]['draws']
@@ -38,10 +38,13 @@ for i in range(6):
 
 iran = {'wins': 0, 'loses': 0, 'draws': 0, 'goal difference': 0, 'points': 0}
 spain = {'wins': 0, 'loses': 0, 'draws': 0, 'goal difference': 0, 'points': 0}
-portugal = {'wins': 0, 'loses': 0, 'draws': 0, 'goal difference': 0, 'points': 0}
-morocco = {'wins': 0, 'loses': 0, 'draws': 0, 'goal difference': 0, 'points': 0}
+portugal = {'wins': 0, 'loses': 0, 'draws': 0,
+            'goal difference': 0, 'points': 0}
+morocco = {'wins': 0, 'loses': 0, 'draws': 0,
+           'goal difference': 0, 'points': 0}
 
-unsortedStandings = {'Iran': iran, 'Spain': spain, 'Portugal': portugal, 'Morocco': morocco}
+unsortedStandings = {'Iran': iran, 'Spain': spain,
+                     'Portugal': portugal, 'Morocco': morocco}
 
 calc(iran, spain, results[0])
 calc(iran, portugal, results[1])
@@ -53,41 +56,34 @@ calc(portugal, morocco, results[5])
 calc_points(unsortedStandings)
 
 standings = list(unsortedStandings.items())
-standings.sort(key=lambda x:x[1]['points'], reverse=True)
+standings.sort(key=lambda x: x[1]['points'], reverse=True)
 
 sorting = list()
 for i in range(len(standings) - 1):
-    print(i)
-    print(standings[i], standings[i][1]['points'])
-    print(standings[i+1], standings[i + 1][1]['points'])
+    iPlusOne = False
     if standings[i][1]['points'] == standings[i + 1][1]['points']:
         if standings[i][1]['wins'] >= standings[i + 1][1]['wins']:
             if standings[i][0] > standings[i + 1][0]:
-                sorting.append(i + 1)
+                iPlusOne = True
+            else:
+                iPlusOne = False
+        else:
+            iPlusOne = False
+    else:
+        iPlusOne = False
+
+    if iPlusOne:
+        sorting.append(i + 1)
     else:
         sorting.append(i)
-    print(sorting)
-    input()
-    
 
-print(sorting)
-sys.exit()
+if iPlusOne:
+    sorting.append(i)
+else:
+    sorting.append(i + 1)
 
-top = 'Iran'
-for i in unsortedStandings:
-    if unsortedStandings[i]['points'] >= unsortedStandings[top]['points'] \
-        and unsortedStandings[i]['wins'] >= unsortedStandings[top]['wins'] \
-        and i > top:
-        top = i
+lastList = [x for _,x in sorted(zip(sorting,standings))]
 
-standings.append({top: unsortedStandings[top]})
-
-for i in unsortedStandings:
-    print(i)
-
-print('================')
-print(results)
-print(unsortedStandings)
-print(standings)
-
-
+for i in lastList:
+    print('%s  wins:%d , loses:%d , draws:%d , goal difference:%d , points:%d' % \
+          (i[0], i[1]['wins'], i[1]['loses'], i[1]['draws'], i[1]['goal difference'], i[1]['points']))
