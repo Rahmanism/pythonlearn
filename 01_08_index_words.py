384d8a37ca46c4ddd1a14db31a1d1075d0a1d27f
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-15 23:11:23 +0330

    Unit 8 is working

--- 01_08_index_words.py
+++ 01_08_index_words.py
@@ -13,3 +13,22 @@ Output:
 18:League
 """
 
+p = input()
+
+words = p.split()
+indices = list()
+previous_was_end_of_sentence = False
+for i in range(1, len(words)):
+    if previous_was_end_of_sentence:
+        previous_was_end_of_sentence = False
+        continue
+    if words[i][-1] == '.':
+        previous_was_end_of_sentence = True
+        words[i] = words[i][:-1]
+    if words[i][-1] == ',':
+        words[i] = words[i][:-1]
+    if ord(words[i][0]) >= 65 and ord(words[i][0]) <= 90:
+        indices.append((i+1,words[i]))
+
+for i in range(len(indices)):
+    print('%i:%s' % (indices[i][0], indices[i][1]))
