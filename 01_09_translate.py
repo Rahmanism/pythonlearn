bdaf6f71983fd454aabb61eadf223ae4a046e2d0
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-15 23:23:22 +0330

    Initial commit for unit 9.

---
+++ 01_09_translate.py
@@ -0,0 +1,16 @@
+# Chapter 01 - Unit 09 - Translation
+
+"""
+Example:
+Input:
+4
+man I je ich
+kheili very très sehr
+alaghemand interested intéressé interessiert 
+barnamenevisi programming laprogrammation Programmierung
+I am very interested in programming
+
+Output:
+man am kheili alaghemand in barnamenevisi
+"""
+
