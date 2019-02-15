deea0178c164957fc87dbca6d6dfd630771eb372
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-15 21:42:34 +0330

    Initial commit for unit 8.

---
+++ 01_08_index_words.py
@@ -0,0 +1,15 @@
+# Chapter 01 - Unit 08 - Count of words starting with capital.
+
+"""
+Example:
+Input:
+The Persian League is the largest sport event dedicated to the deprived areas of Iran. The Persian League promotes peace and friendship.This video was captured by one of our heroes who wishes peace.
+
+Output:
+2:Persian
+3:League
+15:Iran
+17:Persian
+18:League
+"""
+
