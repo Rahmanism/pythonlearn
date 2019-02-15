53ada78ac0022b38621cddb589aa71fd3bc29ed8
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-15 19:42:23 +0330

    Example added in 01_07..

--- 01_07_olympiad.py
+++ 01_07_olympiad.py
@@ -1,2 +1,18 @@
 # Chapter 01 - Unit 07 - List of people registered in Olympiad
 
+"""
+Example:
+Input: 
+4
+m.hosSein.python
+f.miNa.C
+m.aHMad.C++
+f.Sara.java
+
+Output:
+f Mina C
+f Sara java
+m Ahmad C++
+m Hossein python
+"""
+
