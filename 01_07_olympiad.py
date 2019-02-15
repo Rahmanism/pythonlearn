5378095038c57524d23e9ac08718ea8979eef0b9
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-15 21:21:34 +0330

    It seems working.

--- 01_07_olympiad.py
+++ 01_07_olympiad.py
@@ -16,3 +16,25 @@ m Ahmad C++
 m Hossein python
 """
 
+number_of_people = int(input())
+
+data = list()
+for i in range(number_of_people):
+    data.append(input())
+
+for i in range(number_of_people):
+    data[i] = data[i].split('.')
+    data[i][0] = data[i][0].lower()
+    data[i][1] = data[i][1].lower()
+    data[i][1] = data[i][1][0].upper() + data[i][1][1:]
+
+data.sort(key=lambda x: x[0][0])
+
+for i in range(number_of_people):
+    for j in range(number_of_people-i-1):
+        if data[j][0] == data[j+1][0]:
+            if data[j][1] > data[j+1][1]:
+                data[j], data[j+1] = data[j+1], data[j]
+
+for i in range(number_of_people):
+    print('%s %s %s' % (data[i][0], data[i][1], data[i][2]))
