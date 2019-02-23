5fd1d2d50963be4fbc0c986b9a6370ec33f9ac1f
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-23 22:55:14 +0330

    chapter 4 unit 6 completed.

--- 04_02.py
+++ 04_02.py
@@ -1,5 +1,7 @@
 # Chapter 04
 import requests
+from bs4 import BeautifulSoup
+import re
 
 try:
     print('before request')
@@ -7,3 +9,22 @@ try:
     print(r)
 except:
     print('test')
+
+# get bitcoin price from livecoin using API
+r = requests.get('https://api.livecoin.net/exchange/ticker?currencyPair=BTC/USD')
+price = r.json()
+print('highest price of bitcoin: %.2f' % price['high'])
+
+# pip install  beautifulsoup4
+r = requests.get('https://bama.ir/car/all-brands/all-models/all-trims?price=20-50')
+soup = BeautifulSoup(r.text, 'html.parser')
+# find first h2
+val = soup.find('h2')
+# this is equal to following two lines
+all_cars = soup.find_all('h2')
+val = all_cars[0]
+print(val.attrs['class'])
+print(re.sub(r'\s+', ' ', val.text).strip())
+
+for car in all_cars:
+    print(re.sub(r'\s+', ' ', car.text).strip())
