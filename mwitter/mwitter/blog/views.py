d9f7b8439122e30a549837e1fb2c1f93b0c657d1
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-26 16:36:33 +0330

    Now we have a Post table, and a view that shows records of it.

--- mwitter/mwitter/blog/views.py
+++ mwitter/mwitter/blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 def index(request):
-    return HttpResponse("Hi there.")
+    ret = '<div>'
+    all_posts = Post.objects.all()
+    for post in all_posts:
+        ret += '<p>' + post.text + '</p>\n'
+    ret += '</div>'
+    return HttpResponse(ret)
