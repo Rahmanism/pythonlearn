36cd6cf81834706829de05df5f9ff4192a2d55f5
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-26 23:15:52 +0330

    es.py to get estekhare added!

---
+++ es.py
@@ -0,0 +1,15 @@
+# test request for estekhare
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get('http://estekhare.net/index2.php')
+soup = BeautifulSoup(r.text, 'html.parser')
+resultContainer  = soup.find('div', {'id':'newboxes1'})
+result = resultContainer.find('h1')
+resultComment = resultContainer.find('h2')
+address = resultContainer.find('h6')
+print(result.text)
+if len(sys.argv) > 1 and sys.argv[1] == '-c':
+    print(resultComment.text.replace('\n', ''))
+    print(address.text)
