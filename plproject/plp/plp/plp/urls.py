a5bb124274c694307bef47c9896c7149806f6bad
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-27 13:32:04 +0330

    a comma was missed! in plp/urls.py

--- plproject/plp/plp/plp/urls.py
+++ plproject/plp/plp/plp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('plproject/', include('plproject.urls'))
+    path('plproject/', include('plproject.urls')),
     path('admin/', admin.site.urls),
 ]
