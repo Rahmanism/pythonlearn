d660c3d82bb75222d6dc7e177dc99fa044f3ec05
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-02-28 16:13:05 +0330

    API keys for twitter added in comments.

--- gettweet.py
+++ gettweet.py
@@ -1,4 +1,14 @@
 # Gets some tweets with the given hashtag
+
+# 1. We are going to collect tweets with special hashtags for building a good enough data set for a linguistics project.
+# 2. We use the data set for machine learning mechanism, both with supervised and unsupervised methods.
+# 3. Conceptual metaphor analyses may use the results of our project to come up with how people understand and talk about different things.
+# App use description: We will search tweets with special hashtags to create data sets that will be used for machine learning in the field of cognitive linguistics.
+# API key: hBj4e0K5T7VJnVbrGeMwJ7Gze
+# API secret key: gCiZRdcQQqbUCKSXndapOKf88njN670hQLLhHzsociEMEYzWxL 
+# Access token: 3722918664-9ZVGcOFWUuHnuhXQCw5x292zM2bLpXNZfXP94zI
+# Access token secret: YLLoZoyG8gs27nKLaR71HTETATinAqs6QDLEkdBQPwyFG
+
 import sys
 import requests
 from bs4 import BeautifulSoup
@@ -40,10 +50,25 @@ create table if not exists tweets (
 cur.execute(query)
 # end of creating DB and table
 
-hashtag = input('What\'s the hashtag you looking for? ')
-url = 'https://twitter.com/hashtag/%s' % hashtag
-r = requests.get(url)
+if len(sys.argv) > 1:
+    hashtag = sys.argv[1]
+else:
+    hashtag = input('What\'s the hashtag you looking for? ')
+url = 'https://twitter.com/hashtag/%s?lang=fa' % hashtag
+headers = {
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36',
+    'Accept-Encoding':'utf-8'
+}
+r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, 'html.parser')
-tweets = soup.fnid_all('div', {'class': 'tweet'})
-print(tweets.text)
+tweets = soup.find_all('div', {'class': 'tweet'})
+f = open('t.htm', 'w', encoding='utf-8')
+f.write(str(soup.contents))
+f.close()
 print(len(tweets))
+i = 0
+for tweet in tweets:
+    tweet_content = tweet.findChildren('div',{'class':'js-tweet-text-container'})
+    i += 1
+    print(i)
+    print(u"%s" % tweet_content[0].text)
