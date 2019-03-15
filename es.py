0dcdb206c09c50f9891b18e36e9f4eb3dcba2069
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-03-15 20:09:13 +0330

    estekhare moved to funtion in es.py

--- es.py
+++ es.py
@@ -3,14 +3,25 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get('http://estekhare.net/index2.php')
-soup = BeautifulSoup(r.text, 'html.parser')
-resultContainer  = soup.find('div', {'id':'newboxes1'})
-result = resultContainer.find('h1')
-resultComment = resultContainer.find('h2')
-address = resultContainer.find('h6')
-print(result.text)
-if len(sys.argv) > 1 and sys.argv[1] == '-c':
-    print(resultComment.text.replace('\n', ''))
-    print(address.text)
-input()
+def estekhare(show_comment):
+	r = requests.get('http://estekhare.net/index2.php')
+	soup = BeautifulSoup(r.text, 'html.parser')
+	resultContainer  = soup.find('div', {'id':'newboxes1'})
+	result = resultContainer.find('h1')
+	resultComment = resultContainer.find('h2')
+	address = resultContainer.find('h6')
+	ret = result.text
+	if show_comment:
+		ret += '\n%s' % resultComment.text.replace('\n', '')
+		ret += '\n%s' % address.text
+	return ret
+
+again = True
+while again:
+	showComment = len(sys.argv) > 1 and sys.argv[1] == '-c'
+	print(estekhare(showComment))
+	again_input = input('\nAgain (y/N)? ')
+	again = again_input.lower() == 'y'
+	print('\n')
+	
+print('bye')
