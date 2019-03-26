6ba9375db0764f0b56ef7e5e8479b3c9c1004bbf
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-03-26 10:35:45 +0430

    Converted to class!

--- wxlearn/hello.py
+++ wxlearn/hello.py
@@ -5,6 +5,11 @@ import wx
 class MyFrame(wx.Frame):
     def __init__(self):
         super().__init__(parent=None, title='Hello World')
+        panel = wx.Panel(self)
+
+        self.textCtrl = wx.TextCtrl(panel, pos=(5, 5,))
+        myBtn = wx.Button(panel, label='Press Me', pos=(5, 55))
+
         self.Show()
 
 
