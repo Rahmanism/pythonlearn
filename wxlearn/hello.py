234580f81f1c707930f287c146978a43992beb14
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-03-26 11:40:54 +0430

    Text has been typed in text will be shown in a staticctrl (label).

--- wxlearn/hello.py
+++ wxlearn/hello.py
@@ -7,10 +7,32 @@ class MyFrame(wx.Frame):
         super().__init__(parent=None, title='Hello World')
         panel = wx.Panel(self)
 
-        self.textCtrl = wx.TextCtrl(panel, pos=(5, 5,))
-        myBtn = wx.Button(panel, label='Press Me', pos=(5, 55))
+        mySizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.textCtrl = wx.TextCtrl(panel)
+        mySizer.Add(self.textCtrl, 0, wx.ALL | wx.EXPAND, 5)
+
+        myBtn = wx.Button(panel, label='Press Me')
+        myBtn.Bind(wx.EVT_BUTTON, self.onPress)
+        mySizer.Add(myBtn, 0, wx.ALL | wx.ALIGN_RIGHT, 5)
+
+        self.msgTxt = wx.StaticText(panel)
+        mySizer.Add(self.msgTxt, 0, wx.ALL | wx.EXPAND, 5)
+
+        panel.SetSizer(mySizer)
 
         self.Show()
+
+    def onPress(self, event):
+        value = self.textCtrl.GetValue()
+        msg = self.msgTxt.GetLabelText() # .GetValue()
+        if not value:
+            msg += "You didn't enter anything!\n"
+        else:
+            msg += f'You typed: {value}\n'
+        
+        self.msgTxt.SetLabelText(msg) # SetValue(msg)
+        self.textCtrl.SetFocus()
 
 
 if __name__ == '__main__':
