cb163f666d5cbc34739d2c22497f87fcb0772ad0
s.mostafa.rahmani@gmail.com <s.mostafa.rahmani@gmail.com>
2019-04-28 09:21:20 +0430

    decorative functions and fibonacci

--- learning-6.py
+++ learning-6.py
@@ -20,3 +20,48 @@ next(b)
 #%%
 r = range(10)
 list(r)
+
+#%%
+def memo(f):
+    cache = {}
+    def _f(*a):
+        try:
+            return cache[a]
+        except KeyError:
+            cache[a] = f(*a)
+            return cache[a]
+    return _f
+
+@memo
+def fib1(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib1(n-1) + fib1(n-2)
+
+#%%
+fib1(5005)
+
+
+#%%
+@memo
+def fib2(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    n1 = 0
+    n2 = 1
+    c = 1
+    for i in range(n-1):
+        c = n2 + n1
+        n1 = n2
+        n2 = c
+    return c
+
+
+#%%
+fib2(8500)
+
+#%%
